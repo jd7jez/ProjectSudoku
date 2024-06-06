4e9d917d29897151e3466ccb720c68f128535ce9
jd7jez <jd@issaquahsports.com>
2024-06-05 21:26:17 -0700

    Made sudoku board generator

--- SudokuBoard.py
+++ SudokuBoard.py
@@ -1,8 +1,8 @@
 from sudoku import Sudoku
-
+import random
 
 def create_sudoku(difficulty=0.5):
-    # Generate a Sudoku board
+    # Generate a Sudoku board with specified difficulty
     board = Sudoku(3).difficulty(difficulty)
     return board
 
@@ -13,31 +13,27 @@ def print_sudoku_board(board):
         print(type(row))
         print(row)
 
+def generate_board_pair():
+    # Get random difficulty
+    difficulty = random.uniform(0, 1)
 
-def play_sudoku():
-    # Create a Sudoku board
-    board = create_sudoku(difficulty=0.5)
+    # Create a board with the random difficulty
+    board = Sudoku(3).difficulty(difficulty)
 
-    print("Welcome to Sudoku!")
-    print("Here's your Sudoku board:")
-    print_sudoku_board(board)
-    board.show_full()
-    board.solve().show_full()
+    # Save copy of initial board
+    unsolved = list(board.board)
 
-    # while not board.is_solved():
-    #     row = int(input("Enter row (1-9): ")) - 1
-    #     col = int(input("Enter column (1-9): ")) - 1
-    #     value = int(input("Enter value (1-9): "))
-    #
-    #     if board.is_valid_move(row, col, value):
-    #         board.make_move(row, col, value)
-    #         print("Move accepted.")
-    #         print_sudoku_board(board)
-    #     else:
-    #         print("Invalid move. Try again.")
-
-    print("Congratulations! You solved the Sudoku puzzle.")
+    # Attempt to solve the board
+    try:
+        # If successfully solved then return the unsolved and solved boards
+        solved = list(board.solve().board)
+        return (unsolved, solved)
+    except:
+        # Failed to solve the board so recursively try again
+        return generate_board_pair()
 
 
 if __name__ == "__main__":
-    play_sudoku()
+    boards = generate_board_pair()
+    print(boards[0])
+    print(boards[1])
