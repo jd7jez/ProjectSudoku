Fixed one hot encoding in SudokuBoard, made SudokuDQN use a prioritized memory training scheme as well as action masking to choose only valid actions. Created a PrioritizedMemory data structure.

SudokuBoard.py
@@ -47,12 +47,14 @@ class SudokuBoard:
     # I don't want the model to interpret any sort of quantitative relationship between numbers, strictly ordinal
     def one_hot_encode(self, boards):
         # Create numpy array of proper shape
-        enc_boards = np.zeros((boards.shape[0], 81, 9))
+        enc_boards = np.zeros((boards.shape[0], 81, 10))
         # One hot encode the values
         for i, board in enumerate(boards):
-            for j, box in enumerate(board):
-                if box != 0:
-                    enc_boards[i][j][box - 1] = 1
+            for j, val in enumerate(board):
+                if val != None:
+                    enc_boards[i][j][val] = 1
+                else:
+                    enc_boards[i][j][0] = 1
         # Return one hot encoded boards
         return enc_boards
 
@@ -228,8 +230,8 @@ class SudokuBoard:
 if __name__ == "__main__":
     start = time.time()
     sb = SudokuBoard()
-    unsolved, solved, missing = sb.generate_board_pairs(num_boards=10000, difficulty=1)
-    sb.write_board_file(unsolved, solved, 'sudoku-10k-1missing.csv')
+    unsolved, solved, missing = sb.generate_board_pairs(num_boards=10000, difficulty=50)
+    sb.write_board_file(unsolved, solved, 'sudoku-10k-50missing.csv')
     end = time.time()
     print(f"Total Time to generate and save 10000 boards:{end - start}")
 

SudokuDQN.py
@@ -2,12 +2,12 @@ from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Flatten
 from tensorflow.keras.optimizers import Adam
-from collections import deque
 from SudokuGame import SudokuGame
+from SudokuBoard import SudokuBoard
 import numpy as np
-import random
 import time
 import matplotlib.pyplot as plt
+from PrioritizedMemory import PrioritizedMemory
 
 class SudokuDQN:
 
@@ -15,7 +15,7 @@ class SudokuDQN:
         self.state_size = (9, 9)
         self.actions = [(row, col, val) for row in range(9) for col in range(9) for val in range(1, 10)]
         self.action_size = 81 * 9
-        self.memory = deque(maxlen=2000)
+        self.memory = PrioritizedMemory()
         self.gamma = gamma
         self.epsilon = epsilon
         self.epsilon_min = 0.01
@@ -27,65 +27,69 @@ class SudokuDQN:
 
     def fresh_model(self, lr=0.001):
         model = Sequential()
-        model.add(Flatten(input_shape=(9, 9)))
+        model.add(Flatten(input_shape=(81, 10)))
         model.add(Dense(256, activation='relu'))
         model.add(Dense(128, activation='relu'))
         model.add(Dense(self.action_size, activation='linear'))
         model.compile(optimizer=Adam(learning_rate=lr), loss='mse')
         return model
 
-    def memorize(self, board, action, reward, next_state, done):
-        self.memory.append((board, action, reward, next_state, done))
-
-    def act(self, board):
+    def act(self, board, valid_actions_mask):
         if np.random.rand() < self.epsilon:
-            return random.choice(range(self.action_size))
-        act_values = self.model.predict(board, verbose=0)
-        return np.argmax(act_values[0])
+            return np.random.choice(np.where(valid_actions_mask == 1)[0])
+        predictions = self.model.predict(board, verbose=0)[0]
+        predictions[valid_actions_mask == 0] = float('-inf')
+        return np.argmax(predictions)
 
     def replay(self, batch_size):
-        minibatch = random.sample(self.memory, batch_size)
-        for board, action, reward, next_board, done in minibatch:
+        experiences, indices, weights = self.memory.sample(batch_size=batch_size)
+        errors = np.zeros(batch_size)
+        for i, (board, action, reward, next_board, done) in enumerate(experiences):
             target = reward
             if not done:
-                target = (reward + self.gamma * np.amax(self.model.predict(next_board, verbose=0)[0]))
+                target += self.gamma * np.amax(self.model.predict(next_board, verbose=0)[0])
             target_f = self.model.predict(board, verbose=0)
+            errors[i] = abs(target_f[0][action] - target)
             target_f[0][action] = target
-            self.model.fit(board, target_f, epochs=1, verbose=0)
+            self.model.fit(board, target_f, sample_weight=np.reshape(weights[i], (1,)), epochs=1, verbose=0)
+
+        self.memory.update_priorities(indices, errors)
 
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
     def train(self, filename=None, rewards=[10, 50, -10, -50], num_boards=50, batch_size=32):
         game = SudokuGame(preload=True, filename=filename, num_boards=num_boards, reward=True, rewards=rewards, verbose=0)
+        sb = SudokuBoard()
 
-        for n in range(100, num_boards+100):
+        for n in range(num_boards):
             game.setBoard()
-            board = np.reshape(game.get_current(), (1, 9, 9)).astype('float32')
+            valid_actions_mask = self.get_valid_actions(game.get_current())
+            board = sb.one_hot_encode(np.reshape(game.get_current(), (1, 81)))
             total_reward = 0
             start = time.time()
             steps = 0
 
-            for t in range(25):
+            for t in range(5):
                 steps += 1
-                action_index = self.act(board)
+                action_index = self.act(board, valid_actions_mask)
                 action = self.actions[action_index]
                 row, col, val = action
                 reward, code = game.makeMove(row, col, val)
-                next_board = np.reshape(game.get_current(), (1, 9, 9)).astype('float32')
+                next_board = sb.one_hot_encode(np.reshape(game.get_current(), (1, 81)))
                 done = code == 1
                 total_reward += reward
-                self.memorize(board, action_index, reward, next_board, done)
+                self.memory.memorize((board, action_index, reward, next_board, done))
 
                 if code == 2:
                     game.undo_move()
-                board = np.reshape(game.get_current(), (1, 9, 9)).astype('float32')
+                board = sb.one_hot_encode(np.reshape(game.get_current(), (1, 81)))
 
                 if done:
-                    print(f"Episode {n + 1}/{num_boards} finished in {t + 1} steps with total reward {total_reward}")
+                    print(f"SUCCESSFULLY SOLVED BOARD {n + 1}/{num_boards} finished in {t + 1} steps with total reward {total_reward}")
                     break
 
-                if len(self.memory) > batch_size:
+                if self.memory.memory_size > batch_size:
                     self.replay(batch_size)
             self.reward_history.append(total_reward)
             self.steps_history.append(steps)
@@ -93,8 +97,19 @@ class SudokuDQN:
             end = time.time()
             print(f"Finished training on board {n + 1} in {end - start} seconds with a reward of {total_reward}")
 
-            if (n + 1) % 10 == 0:
-                self.save_model(f"sudoku-dqn-{n+1}.h5")
+            if (n + 1) % 50 == 0:
+                self.save_model(f"sudoku-dqn1.1-{n+1}.h5")
+                self.save_history(f"dqn-history1.1-{n+1}ep.txt")
+
+    def get_valid_actions(self, board):
+        valid_actions = np.ones((self.action_size))
+        for y, row in enumerate(board):
+            for x, val in enumerate(row):
+                if val != None:
+                    first_action = (y * 81) + (x * 9)
+                    for i in range(first_action, first_action+9):
+                        valid_actions[i] = 0
+        return valid_actions
 
     def set_model(self, model):
         self.model = model
@@ -112,14 +127,13 @@ class SudokuDQN:
         return self.steps_history
 
     def load_history(self, filename):
-        rewards = []
-        steps = []
         with open(filename, 'r') as file:
             rewards_line = file.readline()
             steps_line = file.readline()
             rewards = map(int, rewards_line.split(','))
             steps = map(int, steps_line.split(','))
-        return list(rewards), list(steps)
+            self.reward_history = list(rewards)
+            self.steps_history = list(steps)
 
     def save_history(self, filename):
         with open(filename, 'w') as file:
@@ -148,9 +162,7 @@ class SudokuDQN:
         plt.show()
 
 if __name__ == "__main__":
-    model = SudokuDQN(epsilon=0.5)
-    model.load_model('sudoku-dqn-100.h5')
-    model.train(filename='sudoku-10k-1missing.csv', num_boards=100)
-    model.save_history('dqn-history-200ep.txt')
+    model = SudokuDQN(epsilon_decay=0.995, gamma=0)
+    model.train(filename='sudoku-10k-1missing.csv', num_boards=10000)
     model.plot_reward_history()
     model.plot_steps_history()